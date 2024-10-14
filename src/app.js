de0d53fd6809a9cad406a0a425255103e03ab9e4
Lucas <contatolucasoficial0@gmail.com>
2024-10-14 16:07:14 -0300

    react start

--- src/app.js
+++ src/app.js
@@ -1,13 +1,24 @@
 const express = require('express')
 let app = express()
 const cors = require('cors')
+const axios = require('axios')
+const dotenv = require('dotenv')
+
+dotenv.config()
 
 app.use(cors)
 
-app.get('/api/home', (req, res) => {
+app.get('/api/home', async (req, res) => {
+    try {
+        const response = await axios.get(`${process.env.REACT_HOME_URL}`);
+        res.json(response.data);
+    } catch (error) {
+        console.error('Erro na chamada:', error);
+        res.status(500).send('Erro ao acessar a página do React');
+    }
+});
 
-})
-
-app.listen(3000, () => {
-    console.log("Servidor ativo na porta 3000!")
-})
+const PORT = parseInt(process.env.PORT) || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
